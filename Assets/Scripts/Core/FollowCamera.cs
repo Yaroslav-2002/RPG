f65ae821f54bb9a999d57adc52b316401747a809
Yaroslav Turchenko <yaroslav.turchenko@tellurionmobile.com>
2022-04-22 02:08:10 +0300

    camera fix

--- Assets/Scripts/Core/FollowCamera.cs
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace RPG.Core
@@ -5,11 +6,20 @@ namespace RPG.Core
     public class FollowCamera : MonoBehaviour
     {
         [SerializeField] private Transform player;
-   
+        private Vector3 Startposition { get; set; }
+        private Vector3 PlayerStartposition { get; set; }
+
+        private void Awake()
+        {
+            Startposition = transform.position;
+            PlayerStartposition = player.position;
+        }  
+        
+
         private void LateUpdate()
         {
-            transform.position = player.position;
-      
+            var position = player.position;
+            transform.position = new Vector3((Startposition.x+ + position.x-  PlayerStartposition.x),(Startposition.y+ + position.y-PlayerStartposition.y),(Startposition.z+ + position.z - PlayerStartposition.z) );
         }
     }
 }
